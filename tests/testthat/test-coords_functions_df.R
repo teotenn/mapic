ee2adc2ef16ae74b3e44220af52b0ef510ac5a60
Manuel Teodoro <teoten@gmail.com>
2023-09-18 16:07:11 +0200

    UT main test improved

--- tests/testthat/test-coords_functions_df.R
+++ tests/testthat/test-coords_functions_df.R
@@ -29,18 +29,18 @@ test_that("coords_from_city: Found results", {
                                   silent = TRUE)
   ## TESTS
   expect_s3_class(found, "data.frame")
-  expect_equal(ncol(found), 3)
+  expect_true(complete.cases(found))
   expect_s3_class(found_state, "data.frame")
-  expect_equal(ncol(found_state), 3)
+  expect_true(complete.cases(found_state))
   expect_s3_class(found_region, "data.frame")
-  expect_equal(ncol(found_region), 3)
+  expect_true(complete.cases(found_region))
 })
 
 
 test_that("coords_from_city: Not found results", {
   ## Case:
-  ## No results found
-  not_found <- coords_from_city(city = t_dat$City[1],
+  ## Not found coords due to wrong city
+  not_found <- coords_from_city(city = "Mex. City",
                                 country_code = t_dat$Country[1],
                                 state = t_dat$Region[1],
                                 silent = TRUE)
@@ -48,13 +48,13 @@ test_that("coords_from_city: Not found results", {
   ## Not found coords due to wrong state or region
   not_found_state <- coords_from_city(city = t_dat$City[1],
                                       country_code = t_dat$Country[1],
-                                      state = t_dat$Region[1],
+                                      state = "Tamaulipas",
                                       silent = TRUE)
   ## TESTS
   expect_s3_class(not_found, "data.frame")
-  expect_equal(ncol(not_found), 3)
+  expect_false(complete.cases(not_found))
   expect_s3_class(not_found_state, "data.frame")
-  expect_equal(ncol(not_found_state), 3)
+  expect_false(complete.cases(not_found_state))
 })
 
 
@@ -82,10 +82,12 @@ test_that("db_load: returns a data frame", {
     db_as_df <- db_load(mock_mdb)
     ## TESTS
     expect_s3_class(db_as_df, "data.frame")
+    expect_equal(nrow(db_as_df), 10)
     expect_equal(nrow(filter(db_as_df, is.na(lat))), 4)
     expect_equal(ncol(db_as_df), 9)
     expect_vector(db_as_df$City, ptype = character())
     expect_setequal(unique(db_as_df$Country), "MX")
+    expect_equal(length(complete.cases(db_as_df)[complete.cases(db_as_df) == TRUE]), 6)
 })
 
 
@@ -101,11 +103,13 @@ test_that("db_remove_empty", {
 test_that("db_compare_data: error", {
     expect_error(db_compare_data(mock_mdb, c(1, 2, 3)),
                  "no applicable method")
+    expect_error(db_compare_data(1, data.frame()),
+                 "Object of class numeric not recognized")
 })
 
 
 test_that("db_compare_data: returns a data frame", {
-    missing <- db_compare_data(mock_mdb, t_dat)
+    missing <- db_compare_data(mock_mdb, mock_data)
     ## TESTS
     expect_s3_class(missing, "data.frame")
     expect_equal(nrow(missing), 4)
@@ -117,7 +121,7 @@ test_that("db_compare_data: returns a data frame", {
 
 test_that("db_join_original_data: from data.frame", {
     combined <- db_join_original_data(mdb = mock_mdb,
-                                      original_data = t_dat)
+                                      original_data = mock_data)
     ## TESTS
     expect_s3_class(combined, "data.frame")
     expect_equal(nrow(combined), 6)
@@ -129,13 +133,13 @@ test_that("db_join_original_data: from data.frame", {
 
 
 test_that("add_coords_manually", {
-    to_add <- data.frame(ID = 1, City = "Ciudad de Mexico",
+    to_add <- data.frame(ID = 1, City = "CD Mex",
                          Country = "MX", Region = "",
                          State = "Mexico", County = "",
                          osm_name = "", lon = 12, lat = 13)
     add_coords_manually(to_add, mock_mdb)
     df_added <- db_load(mock_mdb)
-    api_to_db(mock_mdb, t_dat, state = "Region", silent = TRUE)
+    api_to_db(mock_mdb, mock_data, state = "Region", silent = TRUE)
     df_complete <- db_load(mock_mdb)
     expect_equal(nrow(df_added), 7)
     expect_equal(nrow(df_complete), 10)
